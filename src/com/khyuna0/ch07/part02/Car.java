81d6ff5e478fe7fc3b41905cecae0618207cc1d6
khyuna0 <khohyuna0102@gmail.com>
2025-07-25 13:53:53 +0900

    테스트

--- src/com/khyuna0/ch07/part02/Car.java
+++ src/com/khyuna0/ch07/part02/Car.java
@@ -1,26 +1,23 @@
 package com.khyuna0.ch07.part02;
 
-public class Car {
+public class Car { 
 	
-	// 필드
+	//필드
 	String carName;
 	int maxSpeed;
 	int year;
 	int carPrice;
 	
-//	Tire frontTire = new KumhoTire(); // 앞 -> 금호
-//	Tire rearTire  = new HankookTire(); // 뒤 -> 한국
-
-	Tire frontTire;
-	Tire rearTire;
+//	Tire frontTire = new KumhoTire(); //앞바퀴->금호타이어
+//	Tire rearTire = new HankookTire(); //뒤바퀴->한국타이어
 	
-	// 매개변수의 인수로 금호타이어 객체가 들어오면 "금호" , 한국타이어 객체가 들어오면 "한국" 출력하는 메소드
+	Tire frontTire; //앞 바퀴
+	Tire rearTire; //뒤 바퀴
 	
-	public void tirePrint(Tire tire) {  
-		// 타이어의 이름 출력
+	//매개변수에 인수로 금호타이어 객체가 들어오면->금호, 한국타이어 객체가 들어오면->한국 식으로 출력하는 메소드
+	public void tirePrint(Tire tire) { 
+		//타이어의 이름을 출력
 		tire.tireName();
+		
 	}
-	
-	
-	
 }
