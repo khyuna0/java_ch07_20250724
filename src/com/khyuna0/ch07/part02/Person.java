1abae11c53913fa6dbd67e0d3b7bd75bfc7d9bdd
khyuna0 <khohyuna0102@gmail.com>
2025-07-25 09:50:54 +0900

    1교시

---
+++ src/com/khyuna0/ch07/part02/Person.java
@@ -0,0 +1,10 @@
+package com.khyuna0.ch07.part02;
+
+public class Person {
+	int age;
+	String name;
+	
+	public void namePrint(String name) {
+		System.out.println(name);
+	}
+}
---
+++ src/com/khyuna0/ch07/part02/chap07_Main.java
@@ -0,0 +1,10 @@
+package com.khyuna0.ch07.part02;
+
+public class chap07_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
