77300e4128e1d9ae70edebe4248632545af4fbd5
khyuna0 <khohyuna0102@gmail.com>
2025-07-25 11:48:02 +0900

    다운캐스팅, 업캐스팅

--- src/com/khyuna0/ch07/part02/TireMain.java
+++ src/com/khyuna0/ch07/part02/TireMain.java
@@ -11,14 +11,14 @@ public class TireMain {
 		int tirePrice; // 타이어 종류
 		
 		// new 연산자로 번지수 지정
-		tire = new KumhoTire(); // 자동 형변환
+		tire = new KumhoTire(); // 자동 형변환 -> up casting / 부모에 자식이 들어감
 		
 		// tire.tireCompantKumho -> 자식 필드인 tireCompantKumho는 사용 불가
 		
 		Tire tire2 = new Tire();
 		
-		HankookTire han;
-		han = (HankookTire) tire2; // 강제 형변환
+		KumhoTire kum;
+		kum = (KumhoTire) tire2; // 강제 형변환 -> down casting / 자식에 부모가 들어감
 		
 		// han.tireName = "스노우 타이어"; 
 	}
